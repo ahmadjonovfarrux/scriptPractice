63404b9d573ab72576008e532b4bbd79190213e0
Farrux <Farrux@gmail.com>
2025-01-30 22:45:45 +0500

    Refactor code to enhance readability and maintainability

--- js/app.js
+++ js/app.js
@@ -1,348 +1,132 @@
-// Function ga oid masalalar
-
-// 1.Funksiya yasang, ushbu funksiya 2ta argument qabul qilsin va ularning yig’indisini     qaytarib bersin.
-// function sum(a, b) {
-//   return a + b;
-// }
-// console.log(sum(20, 32));
-
-// 2.Minutlarni butun son sifatida qabul qilib, uni sekundlarga o’girib beradigan funksiya yasang.
-// function timeCalculator(minute) {
-//   return minute * 60;
-// }
-// console.log(timeCalculator(5));
-
-// 3.Funksiya butun son qabul qiladi. Funksiya ushbu butun sondan keyingi sonni qaytarsin.
-// function nextNumber(number) {
-//   return number + 1;
-// }
-// console.log(nextNumber(-2));
-
-// 4.Funskiya uchburchakning asosi va balandligini qabul qiladi. Funksiya uchburchakning yuzini hisoblab qaytarsin. Uchburchak yuzini topish formulasi: S = (asos * balandlik) / 2
-// function triangle(asos, balandlik){
-//     return (asos * balandlik) / 2;
-// }
-// console.log(triangle(5, 6))
-
-// 5.Funksiya yoshni yil ko’rinishida qabul qiladi. ushbu funksiya yoshni kunlarda hisoblab natijani qaytarsin. Bir yil 365 kun deb oling.
-// function ageToDays(age) {
-//   return age * 365;
-// }
-// console.log(ageToDays(25));
-
-// 6.Quyidagi namunalarga muvofiq ravishda funksiyani tana qismini yozing. kub(3) ➞ 27
-// function cubedNumber(number) {
-//   return number ** 3;
-// }
-// console.log(cubedNumber(3));
-
-// 7.Massivni ichidan birinchi elementini qaytaradigan funksiya yasang.
-// let arr = [1, 2, 3, 4, 5];
-// function firstNumber(arr) {
-//   return arr[0];
-// }
-// console.log(firstNumber(arr));
-
-//
-// Function ga oid masalalar
-
-// 1.Funksiya yasang, ushbu funksiya 2ta argument qabul qilsin va ularning yig’indisini     qaytarib bersin.
-// function sum(a, b) {
-//   return a + b;
-// }
-// console.log(sum(20, 32));
-
-// 2.Minutlarni butun son sifatida qabul qilib, uni sekundlarga o’girib beradigan funksiya yasang.
-// function timeCalculator(minute) {
-//   return minute * 60;
-// }
-// console.log(timeCalculator(5));
-
-// 3.Funksiya butun son qabul qiladi. Funksiya ushbu butun sondan keyingi sonni qaytarsin.
-// function nextNumber(number) {
-//   return number + 1;
-// }
-// console.log(nextNumber(-2));
-
-// 4.Funskiya uchburchakning asosi va balandligini qabul qiladi. Funksiya uchburchakning yuzini hisoblab qaytarsin. Uchburchak yuzini topish formulasi: S = (asos * balandlik) / 2
-// function triangle(asos, balandlik){
-//     return (asos * balandlik) / 2;
-// }
-// console.log(triangle(5, 6))
-
-// 5.Funksiya yoshni yil ko’rinishida qabul qiladi. ushbu funksiya yoshni kunlarda hisoblab natijani qaytarsin. Bir yil 365 kun deb oling.
-// function ageToDays(age) {
-//   return age * 365;
-// }
-// console.log(ageToDays(25));
-
-// 6.Quyidagi namunalarga muvofiq ravishda funksiyani tana qismini yozing. kub(3) ➞ 27
-// function cubedNumber(number) {
-//   return number ** 3;
-// }
-// console.log(cubedNumber(3));
-
-// 7.Massivni ichidan birinchi elementini qaytaradigan funksiya yasang.
-// let arr = [1, 2, 3, 4, 5];
-// function firstNumber(arr) {
-//   return arr[0];
-// }
-// console.log(firstNumber(arr));
-
-// 8.Funksiya 2ta argument qabul qilsin. Ushbu funksiya birinchi qiymatning ikkinchi qiymat bilan ko’paytmasini qaytarsin.
-// function multiply(x, y) {
-//   return x * y;
-// }
-// console.log(multiply(3, 6));
-
-// 9.Soat ko’rinishida qiymat kiritilsa, sekundlarda qaytaradigan funksiya yasang.
-// function clock(time) {
-//   return time * 3600;
-// }
-// console.log(clock(3));
-
-// 10.Uchburchakning uchinchi tomonining eng uzun qiymatini qaytaradigan funksiya yasang.
-// function square(tomon1, tomon2) {
-//   return tomon1 + tomon2 - 1;
-// }
-// console.log(square(3, 4));
-
-// 11.Ikkita son argument sifatida kiritilsa, ushbu ikkala sonni birinchisini ikkinchisiga bo’lgandagi qoldiqni qaytarib beradigan funksiya yasang.
-// function qoldiq(son1, son2) {
-//   return son1 % son2;
-// }
-// console.log(qoldiq(3, 4));
-
-// 12.Turtburchakning bo’yi va eni berilsa uning yuzini hisoblab qaytaradigan funksiya yasang. Formula S = bo’yi * eni
-// function yuziniTopish(boyi, eni) {
-//   return boyi * eni;
-// }
-// console.log(yuziniTopish(4, 5));
-
-// 13.Funksiya “a” argument sifatida string ma’lumot qabul qiladi. ushbu funksiya “Something” stringiga " " bo’sh joy va “a” stringini birlashtirib qaytarsin.
-// function addingString(a) {
-//   return "something " + a;
-// }
-// console.log(addingString("is better than nothing"));
-
-// 14.Quyidagi namunalarga qaragan holatda funksiya yasang.
-// function kvadrat(a) {
-//   return a ** 2;
-// }
-// console.log(kvadrat(9));
-
-// 15.Funksiya raqam qabul qiladi. Agar ushbu raqam 0dan kichik yoki teng bo’lsa funksiya rost qaytarsin aks holda yolg’on.
-// function boolean(a) {
-//   if (a <= 0) {
+// Function va arraylarga masalalar
+// 41.Funksiya 2ta argument qabul qiladi. Birinchi argument ikkinchi argumentdan katta emas. Agar birinchi argumentni ikkinchisiga bo’linsa, funksiya true qaytaradi aks holda false
+// function bolinuvchi(x, y) {
+//   if (y % x == 0) {
 //     return true;
 //   } else {
 //     return false;
 //   }
 // }
-// console.log(boolean(2));
+// console.log(bolinuvchi(3, 4));
 
-// 16.Funksiya ko’p  burchakli shaklning burchaklar sonini qabul qiladi. Natijada funksiya ushbu shaklning ichki burchaklar yig’indisini qaytarsin. Formula (n - 2) x 180
-// function findingInerSquare(n) {
-//   return (n - 2) * 180;
+// 42.Funksiyaga raqam string ko’rinishida berilsa, funksiya ushbu ma’lumotni yana raqam ma’lumot turi ko’rinishida qaytarib bersin.
+// function stringNumber(son) {
+//   return Number(son);
 // }
-// console.log(findingInerSquare(4));
+// console.log(stringNumber("23"));
 
-// 17.Basketbol o’yinida ikki ochkolik va uch ochkolik gollar mavjud. Agar funksiya 1-argument sifatida ikki ochkolik gollar sonini va 2-argument sifatida uch ochkolik gollar sonini qabul qilsa, jamoaning jami ochkosini qaytaradigan funksiya yasang.
-// function ochkolar(x, y) {
-//   x = 2;
-//   y = 3;
-//   return x + y;
+// 43.To’rtburchakning yuzini hisoblaydigan funksiya yasang. Bunda funksiyaga to’rtburchakning(ya’ni to’g’ri turtburchak) tomonlari beriladi. Funksiya uning yuzini qaytarishi kerak, agar tomonlar xato kiritilgan bo’lsa funksiya -1 qaytarsin.
+// function findingboxy(x, y) {
+//   if (x > 0 && y > 0) {
+//     return x * y;
+//   } else {
+//     return -1;
+//   }
 // }
-// console.log(ochkolar(1, 2));
+// console.log(findingboxy(1, 4));
 
-// 18.Quyidagi namunalarni kuzatgan holda unga muvofiq funksiya yasang.
-// nameString("Mubashir") ➞ "MubashirEdabit"
-// function addingString(string) {
-//   return string + "Edabit";
+// 44.Funksiyaga ism va familiya argument qilib berilsa, funksiya “ism, familiya” formatdagi string qaytarsin.
+// function ismFamilya(ism, familya) {
+//   return (ism += familya);
 // }
-// console.log(addingString("Mubashir"));
+// console.log(ismFamilya("Farrux ", "Ahmadjonov"));
 
-// 19.Ikkita son kiritilsa, agar ularning yig’indisi 100dan kichkina bo’lsa rost, katta bo’lsa yolg’on qiymat qaytaradigan funksiya yasang.
-// function addingNumber(x, y) {
-//   if (x + y < 100) {
+// 46.Shunday funksiya yasangki, ushbu funksiya 2ta argument qabul qiladi. Birinchi argument massive, ikkinchi argument ushbu massivni boshidan boshlab nechta elementni tushurib qoldirish. Tushirib qoldirilgandan so’ng funksiya natijani qaytarsin.
+// function tushirMassiv(arr, tushurishSoni) {
+//   return arr.slice(tushurishSoni);
+// }
+// console.log(tushirMassiv([1, 2, 3], 1));
+// console.log(tushirMassiv([1, 2, 3, 4, 5], 5));
+
+// 47.Funksiya argument sifatida ovoz berib qo’llab quvvatlashlar soni va ovoz berib qo’llamaganlar sonini qabul qiladi. Funksiya natijani qaytarsin.
+// function voiceCompare(upVoice, downVoice) {
+//   if (upVoice > downVoice) {
+//     return (upVoice -= downVoice);
+//   } else if (upVoice < downVoice) {
+//     return (upVoice -= downVoice);
+//   } else if (upVoice == downVoice) {
+//     return 0;
+//   }
+// }
+// console.log(voiceCompare(2, 2));
+
+// 48.Funksiya son qabul qilsa, ushbu sonning negativini qaytarsin.
+// function teskariSon(son) {
+//   return son * -1;
+// }
+// console.log(teskariSon(0));
+
+// 49.Massivni elementlarini o’rnin almashtiradigan funksiya yasang. Reverse metodi siz ham urinib ko’ring.
+// let secondOne = [];
+// function almash(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     secondOne.push(i);
+//   }
+//   return secondOne;
+// }
+// console.log(almash([1, 2, 3, 4, 5]));
+
+// 50.Kinoteatrga bollar kino ko’rgani kirmoqchi bunda 2ta talab mavjud. Shunda funksiya 2ta argument qabul qiladi. Bolaning yoshini va ota-onasi bilan birgami degan boolean qiymat. Agar bolaning yoshi kamida 15 bo’lsa va ota-onasi bilan birga bo’lsa funksiya true qaytarsin aks holda false.
+// function theatre(yosh, kimBilan) {
+//   if (yosh >= 15 && kimBilan == true) {
 //     return true;
 //   } else {
 //     return false;
 //   }
 // }
-// console.log(addingNumber(32, 27));
+// console.log(theatre(15, true));
 
 // !
-// 20.Quyidagi namunalarni kuzatgan holda unga muvofiq keladigan funksiya yasang.
-// function arrayNumber(number) {
-//   return number[number];
-// }
-// console.log(arrayNumber(3));
-
-// 21.Fermada turli xildagi hayvonlar mavjud. Shunday funksiya yasangki, ushbu funksiya jami fermadagi hayvonlar oyoqlari nechta ekanligini hisoblab qaytarsin. Bunda funksiya birinchi argument sifatida tovuqlar sonini, ikkinchi argument sifatida qo’ylarni va uchinchi argument sifatida sigirlarning sonini qabul qiladi.
-// function legsCalculater(tovuq, qoy, sigir) {
-//   return tovuq * 2 + qoy * 4 + sigir * 4;
-// }
-// console.log(legsCalculater(2, 3, 5));
-
-// 22.Javascriptda “&&” matiqiy operatori mavjud. Ushbu operator ikkita mantiqiy ifoda qabul qiladi. Quyidagi namunaga muvofiq keladigan funskiya yasang. Ushbu operatordan foydalangan holda!
-// function testingOperator(son1, son2) {
-//   if (son1 > 0 && son2 <= 0) {
-//     return true;
-//   } else {
-//     return false;
+// 51.Quyidagi namunalarda kamchilik bor funksiya aslida har bir massivning elementiga 1 qo’shishi kerak. Funksiya to’g’ri yasang.
+// function addingOne(arr) {
+//   for (let i = arr[0]; i <= arr.length; i++) {
+//     arr[i];
 //   }
+//   return arr;
 // }
-// console.log(testingOperator(2, -1));
+// console.log(addingOne([1, 2, 3, 4]));
 
-// 23.Funksiya 2ta son qiymat qabul qiladi. Agar 1-son 2-songa teng bo’lsa rost, aks holda yolg’on qiymat qaytsin. Bunda ikkala qiymatning ma’lumot turi bir xil bo’lsin.
-// function compare(son1, son2) {
-//   if (son1 == son2) {
-//     return true;
-//   } else {
-//     return false;
+//  52.Template string yordamida ya’ni backticlar orqali  `` ushbu formatdagi stringni hosil qiling.
+// let name = "Farrux ";
+// let sureName = "Ahmadjonov";
+// let result = (name += sureName);
+// console.log(result);
+
+// 53.Quyidagi namunani ternary operator ko’rinishida yozing.
+// let son1 = 2;
+// let son2 = 2;
+// if (son1 === son2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let holati = "Yaxshi";
+// if (holati.toLowerCase() == "yaxshi") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+
+
+// 54.Funksiya string qabul qiladi. Agar ushbu stringning uzunligi. juft bo’lsa funksiya true qaytarsin, aks holda false
+
+
+// let hex = ["a", "b", "c", "d", "e", "f", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// function makingColor() {
+//   let stringColor = "#";
+//   for (let i = 0; i < 6; i++) {
+//     stringColor += hex[Math.floor(Math.random() * hex.length)];
 //   }
+//   return stringColor;
 // }
-// console.log(compare(2, 2));
 
-// 24.Futbolda ochkoni hisoblaydigan funksiya yasang. Bunda funksiya yutishlar soni, duranglar soni va mag’lubiyatlar sonini qabul qiladi. 1ta yutish = 3 ochko, 1ta durang = 1 ochko, 1ta mag’lubiyat = 0 ochko hisoblanadi.
-// function futbolochko(yutishlar, duranglar, maglubiyatlar) {
-//   return yutishlar * 3 + duranglar * 1 + maglubiyatlar * 0;
-// }
-// console.log(futbolochko(3, 4, 2));
-
-// 25.Funskiya soatlar va minutlarni argument sifatida qabul qiladi. Ushbu funkisya soatlar va minutlarni sekundga o’girib ularning yig’indisini qaytarsin.
-// function sekundlar(soat, minut) {
-//   return soat * 3600 + minut * 60;
-// }
-// console.log(sekundlar(1, 3));
-
-// !
-// 26.Quyidagi namunalarni kuzatgan holda funksiya yasang.
-// fun(4) ➞ false
-// let counter = 0;
-// function tubSonlar(a) {
-
-//     if(a % )
-// }
-// console.log(tubSonlar(4));
-
-// 27.Funskiay 2ta istalgan turdagi qiymatlar qabul qiladi. Agar ushbu 2ta qiymat ham qiymat jihatidan ham ma’lumot turi jihatidan teng bo’lsa rost, aks holda funkisya yolg’on qiymat qaytarsin.
-// raqam bilan mantiqiy qiymat => bir-biriga teng emas
-// raqam va string => ma'lumot turlari to'g'ri kelmaydi.
-// raqam va raqam => bir-biriga ikkala holatda ham teng
-// function checkingEquality(x, y) {
-//   if ((Number(x) && Boolean(y)) || (Number(y) && Boolean(x))) {
-//     console.log(false);
-//   } else if ((Number(x) && String(y)) || (Number(y) && String(x))) {
-//     console.log(false);
-//   } else if ((Number(x) && Number(y)) || (Number(y) && Number(x))) {
-//     console.log(true);
-//   }
-// }
-// checkingEquality(2, true);
-
-// 28.Funksiya boolean qiymat qabul qiladi. Ushbu funksiya boolean qiymatni stringga o’girib qaytarib bersin.
-// function stringedBool(bool) {
-//   return String(bool);
-// }
-// console.log(stringedBool(true));
-
-// 29.Arrow funksiyasini yarating ushbu funksiya shunchaki berilgan qiymatni qaytarsin.
-// function arrowFunc(index) {
-//   return index;
-// }
-// console.log(arrowFunc('hello'));
-
-// 30.Framelar sonini hisoblaydigan funksiya yarating. Frame bu 1 sekundda necha marta ko’rinishlar soni aylanishi. Funksiya minutlar va 1 sekundda nechta framelar soni aylanishini argument sifatida qabul qiladi. Funksiya jami framelar sonini qaytarsin.
-// function frameSoni(minut, frame) {
-//   return minut * 60 * frame;
-// }
-// console.log(frameSoni(10, 25));
-
-// !
-// 31.Oddiy matematik amallar ketma-ketligi string ko’rinishida funksiyaga argument sifatida beriladi. Ushbu funksiya matematik ifodani bajarib natijani qaytarsin.
-// function calc(stringNumber) {
-//   const number = stringNumber;
-//   return parseInt(stringNumber, 10);
-// }
-// console.log(calc(`4-2`));
-
-// 32.Shunday funksiya yasangki, unda 2ta butun son argument qilib beriladi. Agar ushbu sonlardan birontasi 10ga teng bo’lsa yoki ularning yig’indisi 10ga teng bo’lsa funksiya rost qiymat qaytaradi. Aks hold yolg’on.
-// function equalingTen(son1, son2) {
-//   if (son1 == 10 || son2 == 10 || son1 + son2 == 10) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(equalingTen(1, 9));
-
-// 33.Mashina kilometriga 10litr benzin ichadi. Mashina doim yo’lga chiqishdan oldin kamida 100litr benzin bilan chiqadi. Agar masofa funksiyaga argument sifatida berilsa, ushbu masofaga chiqish uchun Mashina necha litr benzin bilan chiqishi keraglini funksiya qaytarib bersin.
-// function litrMasofa(km) {
-//   return 10 * km;
-// }
-// console.log(litrMasofa(3));
-
-// 34.Quyidagi namunaga nazar tashlagan holda funksiya yasang.
-// function compare(son1, son2) {
-//   if (son1 > son2) {
-//     return son1;
-//   } else {
-//     return son2;
-//   }
-// }
-// console.log(compare(2, 2));
-
-// 35.Funksiya 2ta argument berilsa, funksiya anashu 2ta argumentdan iborat massiv qaytarsin.
-// function massiv(x, y) {
-//   return [x, y];
-// }
-// console.log(massiv(31, 34));
-
-// 36.Funksiyaga 2ta argument sifatida string ko’rinishidagi ma’lumotlar beriladi. Agar ushbu ikkala stringdagi belgilar soni bir-birinikiga teng bo’lsa funksiya rost qiymat qaytarsin, aks hold yolg’on.
-// function tengString(str1, str2) {
-//   if (str1.length == str2.length) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(tengString("hello", "sarq"));
-
-// 37.Shunday funksiya yasangki, unga string argument qilib beriladi, agar ushbu string bo’sh bo’lsa funksiya true qaytaradi, aks holda false.
-// function boshStr(str) {
-//   if (str == "") {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(boshStr("hello"));
-
-// 38.Shunday funksiya yasang.Unda butun son argument qilib beriladi. Agar ushbu butun son 5ga bo’linsa, funksiya true qaytarsin, aks holda false.
-// function beshBoluv(son) {
-//   if (son % 5 == 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(beshBoluv(20));
-
-// 39.Shunday funksiya yasang.Unda butun son argument qilib beriladi. Agar ushbu butun son 100ga bo’linsa, funksiya true qaytarsin, aks holda false.
-// function yuzBoluvchi(son) {
-//   if (son % 100 == 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(yuzBoluvchi(200));
-
-// 40.Shunday funksiya yasangki, ushbu funksiya stringni ichida nechta belgi borligini aytsin. Bunda length propertisidan foydalanmang va rekursiv funksiya ishlating.
-
-// function stringIndex(index) {
-//   return ;
-// }
-// console.log(stringIndex("hello"));
+// let bgColor = `linear-gradient(${makingColor()}, ${makingColor()})`;
+// document.body.style = `
+// background:${bgColor};
+// background-repeat:no-repeat;
+// background-size:cover;
+// height:100vh;
+// `;
