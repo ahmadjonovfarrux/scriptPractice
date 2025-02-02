9ebcfd47f04f991ecf81a08decb39f445af15687
Farrux <Farrux@gmail.com>
2025-02-02 23:34:48 +0500

    Remove commented-out code and placeholder functions from app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
-
 // Array methods
-const numbers = [1, 2, 3, 4, 5];
-// forEach()
-numbers.forEach((son) => {
-  console.log(son ** 2);
-});
+// const numbers = [1, 2, 3, 4, 5];
+// const newNumbers = [];
+// // forEach()
+// numbers.forEach((son) => {
+//   newNumbers.push(son ** 2);
+// });
+// console.log(newNumbers);
+
+// Array1. n natural soni berilgan. Dastlabki n ta toq sondan tashkil topgan massiv qaytaruvchi getInitialOdds(n) nomli funksiya tuzing.
+// function getInitialOdds(n) {
+//   const newNumbers = [];
+//   for (let i = 1; i <= n; i++) {
+//     newNumbers.push(2 * i - 1);
+//   }
+//   return newNumbers;
+// }
+// const result = getInitialOdds(5);
+// console.log(result);
+
+// Array2. arr nomli massiv berilgan. Massiv elementlari orasidan juftlarini indekslari kamayish tartibidan hosil bo’lgan massivni qaytaruvchi getEvenReverse(arr) programma tuzilsin.
+// let arr = [4, 5, 7, 8, 6, 9];
+// let newNumbers = [];
+// const getEvenReverse = (arr) => {
+//   arr.forEach((son) => {
+//     if (son % 2 == 0) {
+//       newNumbers.unshift(son);
+//     }
+//   });
+//   return newNumbers;
+// };
+// const result = getEvenReverse(arr);
+// console.log(result);
+
+// Array3. n ta elementdan tashkil topgan massiv berilgan. Massiv elementlarini quyidagicha chiqaruvchi programma tuzilsin.
+// const arr = [4, 5, 7, 8, 6, 9];
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   result.push(arr[i], arr[arr.length - 1 - i]);
+// }
+// console.log(result.join(" "));
+
+// Array4. N ta elementdan tashkil topgan arr nomli massiv va K, L butun sonlari berilgan. (0 <= K <= L < N). Massivning K va L indekslari orasidagi elementlari yig'indisini chiqaruvchi rangeSum(arr, K, L) programma tuzilsin.
+const arr = [1, 6, 9, 5, 8, 10, 15];
+
